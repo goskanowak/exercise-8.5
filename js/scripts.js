49deaf57740b7f5c715513434f3628ad882f0989
Goska <36965568+goskanowak@users.noreply.github.com>
2018-03-31 00:40:33 +0200

    Change name function

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
-function myFunction(){
+var name= prompt('Enter your name: ');
+alert('Hello ' + name);
+document.getElementById('name').innerHTML = name;
+
+function calculateTriangleArea(){
     document.getElementById('result').innerHTML = '';
     var a = document.getElementById('base').value;
     var h = document.getElementById('height').value;
@@ -6,13 +10,6 @@ function myFunction(){
     var triangleArea = a*h/2;
 
     document.getElementById('result').innerHTML = triangleArea;
-    
-//    alert('The triangle area is: ' + triangleArea);
+
     console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
     }
-var name= prompt('Enter your name: ');
-
-alert('Hello ' + name);
-
-document.getElementById('name').innerHTML = name;
-
